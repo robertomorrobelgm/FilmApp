b9916c84dd96a5fe0ea06ec928609f0833dced9d
roberto_morrobel@claro.com.do <roberto_morrobel@claro.com.do>
2022-01-19 20:04:28 -0400

    Initial Commit

---
+++ FilmApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilmApp
+//
+//  Created by Roberto Morrobel (ClaroDom) on 1/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilmApp/FilmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAppApp.swift
+//  FilmApp
+//
+//  Created by Roberto Morrobel (ClaroDom) on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FilmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
